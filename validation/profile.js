c9553397d28fb096efc1e9fc11df9f6003c33d54
Johnnie Tran <johnnie.tran88@gmail.com>
2018-10-23 15:28:29 -0700

    add education functionality

--- validation/profile.js
+++ validation/profile.js
@@ -1,23 +1,23 @@
-const Validator = require('validator');
-const isEmpty = require('./is-empty');
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
-module.exports = function vadlidateProfileInput(data){
-    let errors = {};
+module.exports = function vadlidateProfileInput(data) {
+  let errors = {};
 
-    data.handle = !isEmpty(data.handle) ? data.handle : '';
-    data.status = !isEmpty(data.status) ? data.status : '';
-    data.skills = !isEmpty(data.skills) ? data.skills : '';
+  data.handle = !isEmpty(data.handle) ? data.handle : "";
+  data.status = !isEmpty(data.status) ? data.status : "";
+  data.skills = !isEmpty(data.skills) ? data.skills : "";
 
-    if(!Validator.islength(data.handle, { min: 2, max: 40 })){
-        errors.handle = 'Handle needs to be between 2 and 4 characters.';
-    }
+  if (!Validator.islength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = "Handle needs to be between 2 and 4 characters.";
+  }
 
-    if(!Validator.isEmpty(data.handle)){
-        errors.handle = 'Profile handle is required.';
-    }
+  if (!Validator.isEmpty(data.handle)) {
+    errors.handle = "Profile handle is required.";
+  }
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
